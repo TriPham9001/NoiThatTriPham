1f4d25844093472ff4a5b4dd6d17366edae09353
TriPham9001 <tri.pham1101@gmail.com.com>
2022-11-02 22:41:22 +0700

    Tri completed 90% of the website

--- js/show-category-left.js
+++ js/show-category-left.js
@@ -1,5 +1,5 @@
-const targetDiv = document.getElementById("third");
-const btn = document.getElementById("toggle");
+const targetDiv = document.getElementById("showColor");
+const btn = document.getElementById("clickColor");
 btn.onclick = function () {
   if (targetDiv.style.display !== "none") {
     targetDiv.style.display = "none";
@@ -9,15 +9,15 @@ btn.onclick = function () {
 };
 
 
-const targetDiv2 = document.getElementById("show-catergory");
-const btn2 = document.getElementById("click-catergory");
+const targetDiv2 = document.getElementById("filter");
+const btn2 = document.getElementById("clickfilter");
 btn2.onclick = function () {
   if (targetDiv2.style.display !== "none") {
     targetDiv2.style.display = "none";
   } else {
-    targetDiv2.style.display = "block";
+    targetDiv2  .style.display = "block";
   }
-};
+};  
 
 
 
